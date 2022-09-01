aafee231617ee6773e26331d4d35544573841eb9
Simao S <112442632+simaosoftware@users.noreply.github.com>
2022-09-01 05:14:05 +0100

    Update calculator.c

--- C (Class 1)/calculator.c
+++ C (Class 1)/calculator.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <cs50.h>
+#include <cs50.h> //bool data type not included without cs50 library | string data type as well
 
 int main(void)
 {
-    long x = get_long("x: ");
-    long y = get_long("y: ");
+    long x = get_long("x: "); //use unsigned int when I want to use the range of positives only, at the expense of no negatives
+    long y = get_long("y: "); //use short for ?? | char uses 1 byte - 8bits - therefore 2^8/2 is 127 positive numbers for ASCII (numbers as strings are different than int)
+// I should also name all variables in the same line separated by comma if of the same data type
+// Here I am initializing (assigning and declaring in one sentence, great. remember after 1st data type declaration, do NOT specify type again)
     printf("%li\n", x+y);
 }
 
-// careful with assigning wrong data type. a machine running 32bits meaning it can only count up to 2^32= 4 billion in one direction. with negatives, 4 billion turn to approx 2 billion up and 2 billion down. so if I assign to x 3B, it will ask me for x again
+// careful with assigning wrong data type. an int uses 4 bytes - 32bits meaning it can only count up to 2^32= 4 billion in one direction. with negatives, 4 billion turn to approx 2 billion up and 2 billion down. so if I assign to x 3B, it will ask me for x again
 // even 64 bits has a cap though
 
 /*
